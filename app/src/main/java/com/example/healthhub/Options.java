655e5d5205ea168ca83f48f7e3e1671f7c89b2c3
nincolperez <perezn2@uhv.edu>
2022-04-15 10:01:16 -0500

    cleaned up some of the code in the options activity

--- app/src/main/java/com/example/healthhub/Options.java
+++ app/src/main/java/com/example/healthhub/Options.java
@@ -12,25 +12,19 @@ public class Options extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_options);
 
-        Button makeAppoinmnet = findViewById(R.id.button3);
+        Button makeAppointment = findViewById(R.id.button3);
 
         Button rx = findViewById(R.id.button4);
 
-        Button labreport = findViewById(R.id.button5);
-
-
-
-
+        Button labReport = findViewById(R.id.button5);
 
         Button logout = findViewById(R.id.logoutButton);
 
-
-
-
         //Take the user to the make appointment activity
-        makeAppoinmnet.setOnClickListener(new View.OnClickListener() {
+        makeAppointment.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent makeAppointment = new Intent(Options.this, Appointment.class);
@@ -38,8 +32,7 @@ public class Options extends AppCompatActivity {
             }
         });
 
-
-        //Take the user to the make appointment activity
+        //Take the user to the prescriptions activity
         rx.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -48,17 +41,14 @@ public class Options extends AppCompatActivity {
             }
         });
 
-        //Take the user to the make appointment activity
-                labreport.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Intent labreport = new Intent(Options.this, ReportsActivity.class);
-                        startActivity(labreport);
-                    }
-                });
-
-
-
+        //Take the user to the lab report activity
+        labReport.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent lab = new Intent(Options.this, ReportsActivity.class);
+                startActivity(lab);
+            }
+        });
 
         //Log out will take user to main screen
         logout.setOnClickListener(new View.OnClickListener() {
@@ -70,12 +60,6 @@ public class Options extends AppCompatActivity {
             }
         });
 
-
-
-
-
-
     }
-
 
 }
